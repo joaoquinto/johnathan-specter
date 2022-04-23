3b25092622dc5a6e8c72fe60fd4d5548b7f03cbf
Joaoquinto <joaovictoroliveirapereira6522@gmail.com>
2022-04-22 22:31:07 -0300

    Adicionando toolkit 2 e 5

--- js/modules/aumentarEspacamento.js
+++ js/modules/aumentarEspacamento.js
@@ -1,14 +1,25 @@
-const tags = ["h1", "h2", "p", "a"];
+const tags = ["h1", "h2", "p", "a", "button"];
 const espacamentoButton = document.querySelector("#aumentarEspacamento");
+const resetarButton = document.querySelector('#resetarEspacamento')
 
-/* NÂO FUNCIONA */
 export function espacamentoLetras() {
   tags.forEach((tag) => {
     const elements = document.querySelectorAll(`${tag}`);
     elements.forEach((element) => {
-      element.style.letterSpacing = 1.5;
-      console.log(getComputedStyle(element).letterSpacing);
+      element.style.letterSpacing = "1.5px";
     });
   });
 }
+
 espacamentoButton.addEventListener("click", espacamentoLetras);
+
+export function resetarEspacamento() {
+  tags.forEach((tag) => {
+    const elements = document.querySelectorAll(`${tag}`);
+    elements.forEach((element) => {
+      element.style.letterSpacing = "unset";
+    });
+  });
+}
+
+resetarButton.addEventListener('click', resetarEspacamento)
--- js/modules/aumentarFonte.js
+++ js/modules/aumentarFonte.js
@@ -2,31 +2,43 @@ const fontSizeButton = document.querySelector('#fontSize')
 const resetarFonteButton = document.querySelector('#resetarFonte')
 const tags = ['h1', 'h2', 'p']
 
-/* Necessário validar a quantidade de vezes que a função vai ser chamada */
-export function fontSize() {
-  const textos = tags
-  textos.forEach((tag) => {
-    const elements = document.querySelectorAll(`${tag}`)
-    elements.forEach(element => {
-      const sizeString = getComputedStyle(element).fontSize
-      const size = parseInt(sizeString)
-      const aumentarFonte = `${size + (size * 0.20)}px`
-      element.style.fontSize = aumentarFonte
+let validator = 0;
+
+export function fontSize(event) {
+  if (validator === 0) {
+    const textos = tags
+    textos.forEach((tag) => {
+      const elements = document.querySelectorAll(`${tag}`)
+      elements.forEach(element => {
+        const sizeString = getComputedStyle(element).fontSize
+        const size = parseInt(sizeString)
+        const aumentarFonte = `${size + (size * 0.2)}px`
+        element.style.fontSize = aumentarFonte
+      })
     })
-  })
+    validator++
+  } else {
+    event.preventDefault()
+  }
 }
 
 
 fontSizeButton.addEventListener('click', fontSize)
 
 export function resetarFonte() {
-  const textos = tags
-  textos.forEach((tag) => {
-    const elements = document.querySelectorAll(`${tag}`)
-    elements.forEach(element => {
-      element.removeAttribute('style')
+  if (validator === 1) {
+    const textos = tags
+    textos.forEach((tag) => {
+      const elements = document.querySelectorAll(`${tag}`)
+      elements.forEach(element => {
+        const sizeString = getComputedStyle(element).fontSize
+        const size = parseInt(sizeString)
+        const resetar = `${size - (size * 0.20)}px`
+        element.style.fontSize = resetar
+      })
     })
-  })
+    validator--
+  }
 }
 
 resetarFonteButton.addEventListener('click', resetarFonte)
